cbd4ae196cafd53627d0707f61528285434dc1f3
ZhiQi Wang <1373319493@qq.com>
2026-09-25 11:56:44 +0800

    Time: 0 ms (100%), Space: 10.5 MB (97.53%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -3,13 +3,10 @@ public:
     std::vector<int> countBits(int n) {
         std::vector<int> ans;
         ans.reserve(n+1);
-        for(int i=0;i<=n;i++){
+        ans.push_back(0);
+        for(int i=1;i<=n;i++){
             int num=0;
-            int x=i;
-            while(x>0){
-                num+=x&1;
-                x=x>>1;
-            }
+            num=ans[i/2]+(i&1);
             ans.push_back(num);
 
         }
